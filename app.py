19dd3a60d83ad2e09ef01204717e5cfa9243ffef
Fatih Kilic <me@fklc.dev>
2025-11-11 10:08:07 -0500

    Clean code and add 1 second threshold to motion detection

--- app.py
+++ app.py
@@ -1,69 +1,89 @@
 import cv2
-from collections.abc import Callable
 import numpy as np
+import time
 
 
-def start_capture_loop(
-    process_frame_callback: Callable[[cv2.typing.MatLike], cv2.typing.MatLike],
-):
-    cap = cv2.VideoCapture(0)  # For other cameras, change the index to 1, 2, etc.
-    # cap = cv2.VideoCapture("rtsp://your_ip_camera_url") # Example for IP camera
-
-    if not cap.isOpened():
-        print("Error: Could not open video.")
-        return
-
-    while True:
-        # Capture frame-by-frame
-        ret, frame = cap.read()
-        if not ret:
-            print("Error: Could not read frame.")
-            break
-
-        # Process the frame using the provided callback function
-        frame = process_frame_callback(frame)
-
-        # Display the resulting frame
-        cv2.imshow("Video Feed", frame)
-
-        # Break the loop on 'q' key press
-        if cv2.waitKey(1) == ord("q"):
-            break
-
-    # Release the capture and close windows
-    cap.release()
-    cv2.destroyAllWindows()
+class State:
+    def __init__(self):
+        self.detected_motion = True
 
 
 class MotionDetector:
-    fgbg = cv2.createBackgroundSubtractorMOG2(
-        history=300, varThreshold=25, detectShadows=True
-    )
+    def __init__(self):
+        self.fgbg = cv2.createBackgroundSubtractorMOG2(
+            history=100, varThreshold=25, detectShadows=True
+        )
+        self.last_motion_time = time.time()
 
-    def detect_motion(frame: cv2.typing.MatLike) -> bool:
-        fgmask = MotionDetector.fgbg.apply(frame)
-        motion = np.sum(fgmask > 0) > 5000  # Could be tuned
+    def detect_motion(self, frame: cv2.typing.MatLike) -> bool:
+        fgmask = self.fgbg.apply(frame)
+        motion = np.sum(fgmask > 200) > 2500  # Could be tuned
+        if motion:
+            self.last_motion_time = time.time()
         return motion
 
-
-def process_frame(frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
-    # Example processing: convert to grayscale
-    gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-
-    motion = MotionDetector.detect_motion(frame)
-    if motion:
-        # Don't move the robot arm if motion is detected
-        pass
-
-    # here we could have something like read_board(frame), detect_hand(frame), etc. etc.
-    # then based on that we could take moves with the robot arm
-
-    return gray_frame
+    def time_since_last_motion(self) -> float:
+        return time.time() - self.last_motion_time
 
 
-def main():
-    start_capture_loop(process_frame)
+class RobotController:
+    def __init__(self):
+        self.state = State()
+        self.motion_detector = MotionDetector()
+
+    def start_capture_loop(self):
+        cap = cv2.VideoCapture(0)  # For other cameras, change the index to 1, 2, etc.
+        # cap = cv2.VideoCapture("rtsp://your_ip_camera_url") # Example for IP camera
+
+        if not cap.isOpened():
+            print("Error: Could not open video.")
+            return
+
+        while True:
+            # Capture frame-by-frame
+            ret, frame = cap.read()
+            if not ret:
+                print("Error: Could not read frame.")
+                break
+
+            frame = self.process_frame(frame)
+
+            # Display the resulting frame
+            cv2.imshow("Video Feed", frame)
+
+            self.take_action()
+
+            # Break the loop on 'q' key press
+            if cv2.waitKey(1) == ord("q"):
+                break
+
+        # Release the capture and close windows
+        cap.release()
+        cv2.destroyAllWindows()
+
+    def process_frame(self, frame: cv2.typing.MatLike) -> cv2.typing.MatLike:
+        # Example processing: convert to grayscale
+        gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+        # Motion detection
+        self.motion_detector.detect_motion(frame)
+        self.state.detected_motion = (
+            self.motion_detector.time_since_last_motion() < 1.0
+        )  # 1 second threshold
+
+        # here we could have something like read_board(frame), detect_hand(frame), etc. etc.
+        # then based on that we could take moves with the robot arm
+
+        return gray_frame
+
+    def take_action(self):
+        if self.state.detected_motion:
+            # Detected motion, not moving
+            return
+
+        # No motion detected, safe to move
 
 
 if __name__ == "__main__":
-    main()
+    robot_controller = RobotController()
+    robot_controller.start_capture_loop()
