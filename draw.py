3a821f9c04747571799352351808846bdc2ac8dd
Daniil <sirdaniiltrukhin@gmail.com>
2025-11-10 22:08:16 -0500

    start repo

---
+++ draw.py
@@ -0,0 +1,4 @@
+# use robotics toolbox to have the robot draw the tic-tac-toe board
+# have it also draw some circles and crosses
+#
+# main goal is to test drawing capabilities of the robot
---
+++ read_board.py
@@ -0,0 +1,1 @@
+# OpenCV code that can read the state of the tic-tac-toe board
