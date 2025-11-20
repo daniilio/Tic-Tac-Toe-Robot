284f3c48cdb9fc6669124954e52874f5998502a3
Radian Gondokaryono <radian.gondokaryono@mail.utoronto.ca>
2025-11-20 13:01:42 -0500

    dt = 0.03

--- test_trajectory_real.py
+++ test_trajectory_real.py
@@ -64,7 +64,7 @@ def joint_space_trajectory(robot: csc376_bind_franky.FrankaJointTrajectoryContro
     q_traj = np.linspace(q_start, q_target, num_points)
 
     # Time step
-    dt = 0.01  # 10 ms between commands
+    dt = 0.03  # 10 ms between commands
     run_on_robot(robot, [q_traj], dt)
 
 
